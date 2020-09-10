8056a0edd174629bf6e15f082f1c4fac857d19dc
Surajv <vsuraj2405@gmail.com>
2020-09-10 13:25:04 +0530

    added code

---
+++ app5styling.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const source = "https://picsum.photos/200";
+ReactDOM.render(
+  <div>
+    <h1 className = "h" >My Favourite Foods</h1>
+   <p> jreforufioer</p>
+   <img alt = "random img" src = {source}/>
+  </div>,
+  document.getElementById("root")
+);
+// you may use html global attributes
+// like contentEditable or spellCheck.....
+// unlike class in html, we use className in jsx
