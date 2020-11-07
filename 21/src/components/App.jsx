35d42435e0a11d7dcd8678af4bce4c80e360a42f
Surajv <vsuraj2405@gmail.com>
2020-11-07 19:39:19 +0530

    useState in React

---
+++ 21/src/components/App.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+function App() {
+  // use state returns two values, -> value & a function 
+  // the 0 passed is stored in count 
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+    </div>
+  );
+}
+
+export default App;
